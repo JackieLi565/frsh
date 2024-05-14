550f6636300ccb16efb517031c372541cf5fb87a
Jackie Li <li.jackie565@gmail.com>
2024-05-14 16:24:52 -0400

    re-work

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+// @ts-check
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
 
-export default [
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-];
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        ignores: ['node_modules/', 'lib/', '.gitignore'],
+    }
+)
---
+++ packages/frsh/src/types.ts
@@ -0,0 +1,17 @@
+export interface Options {
+    // base path of session data
+    sessionPath: string
+    // user session table path
+    tablePath: string
+    // expiration in seconds
+    expires: number
+}
+
+export interface BaseAttributes {
+    // reference to firestore
+    userId: string
+    // session time to live in milliseconds
+    TTL: number
+}
+
+export type WithBaseAttributes<R = {}> = BaseAttributes & R
