3b14eb174b5220f846722a7161ff1836f4fae6ef
Jackie Li <li.jackie565@gmail.com>
2024-05-14 18:14:01 -0400

    0.0.1

--- packages/frsh/src/index.ts
+++ packages/frsh/src/index.ts
@@ -1,2 +1,30 @@
-export * from './core.js'
-export * from './types.js'
+export interface SessionAttributes {}
+
+export interface Session extends SessionAttributes {
+    userId: string
+    TTL: number
+}
+
+export interface Adaptor {
+    getSession(sessionId: string): Promise<Session | null>
+    createSession(session: Session): Promise<Session>
+    updateSessionExpiry(sessionId: string, newExpiry: number): Promise<Session>
+    removeSession(sessionId: string): Promise<void>
+    removeExpiredSessions(): Promise<void>
+    removeUserSessions(userId: string): Promise<void>
+}
+
+export interface Options {}
+
+export class Frsh {
+    private adaptor: Adaptor
+    private options: Options
+
+    constructor(adaptor: Adaptor, options?: Partial<Options>) {
+        this.adaptor = adaptor
+
+        const defaultOptions: Options = {}
+
+        this.options = { ...defaultOptions, ...options }
+    }
+}
