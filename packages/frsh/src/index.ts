d2002c956e0730cb5515fb7c5dd57557691f9203
Jackie Li <li.jackie565@gmail.com>
2024-05-15 00:42:03 -0400

    core impl

--- packages/frsh/src/index.ts
+++ packages/frsh/src/index.ts
@@ -7,6 +7,7 @@ export interface Session extends SessionAttributes {
 
 export interface Adaptor {
     getSession(sessionId: string): Promise<Session | null>
+    getUserSessions(userId: string): Promise<Session[]>
     createSession(session: Session): Promise<Session>
     updateSessionExpiry(sessionId: string, newExpiry: number): Promise<Session>
     removeSession(sessionId: string): Promise<void>
@@ -14,7 +15,18 @@ export interface Adaptor {
     removeUserSessions(userId: string): Promise<void>
 }
 
-export interface Options {}
+export interface PathConfig {
+    // root storage path
+    root: string[]
+    // session path
+    sessions: string[]
+    // table path
+    table: string[]
+}
+
+export interface Options {
+    expiry: number
+}
 
 export class Frsh {
     private adaptor: Adaptor
@@ -23,8 +35,61 @@ export class Frsh {
     constructor(adaptor: Adaptor, options?: Partial<Options>) {
         this.adaptor = adaptor
 
-        const defaultOptions: Options = {}
+        const defaultOptions: Options = {
+            expiry: 60 * 60 * 3, // 3 hours
+        }
 
         this.options = { ...defaultOptions, ...options }
+    }
+
+    async createSession(
+        userId: string,
+        record: SessionAttributes
+    ): Promise<Session> {
+        const session: Session = {
+            userId,
+            TTL: Date.now() + this.options.expiry,
+            ...record,
+        }
+
+        await this.adaptor.createSession(session)
+
+        return session
+    }
+
+    async verifySession(sessionId: string): Promise<Session | null> {
+        const session = await this.adaptor.getSession(sessionId)
+
+        if (!session) return null
+        if (this.isExpired(session.TTL)) {
+            await this.adaptor.removeSession(sessionId)
+            return null
+        }
+
+        return session
+    }
+
+    async getUserSessions(userId: string) {
+        const sessions = await this.adaptor.getUserSessions(userId)
+
+        return sessions
+            .filter((session) => !this.isExpired(session.TTL))
+            .sort((a, b) => b.TTL - a.TTL)
+    }
+
+    async deleteSession(sessionId: string) {
+        await this.adaptor.removeSession(sessionId)
+    }
+
+    async deleteUserSessions(sessionId: string) {
+        await this.adaptor.removeUserSessions(sessionId)
+    }
+
+    async deleteExpiredSessions() {
+        await this.deleteExpiredSessions()
+    }
+
+    private isExpired(expiry: number) {
+        return Date.now() > expiry
     }
 }
